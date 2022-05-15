be07a77b58a91124c5cd011da72b2dec56b9981b
Jimmy Andreas <jimmysignature@gmail.com>
2022-05-14 21:36:53 -0400

    Add accordian

--- index.js
+++ index.js
@@ -1,98 +1,103 @@
-window.addEventListener("DOMContentLoaded", (e) => {
+window.addEventListener('DOMContentLoaded', e => {
+  console.log('connected')
 
-console.log("connected")
+  const selectedColor = document.querySelector('#selectedColor')
+  const colorPicker = document.querySelector('#colorPicker')
 
-    const selectedColor = document.querySelector("#selectedColor")
-    const colorPicker = document.querySelector("#colorPicker")
-    
+  const sectColor = document.querySelector('#sectColor')
+  sectColor.style.display = 'none'
 
-    colorPicker.addEventListener("change", (e) =>{
-        const color = e.target.value
-        // console.log(e.target.value)
-        selectedColor.style.backgroundColor = color
-        const colorLessHash = color.slice(1)
-        // console.log(colorLessHash)
-        
+  colorPicker.addEventListener('change', e => {
+    const color = e.target.value
+    // console.log(e.target.value)
+    selectedColor.style.backgroundColor = color
+    const colorLessHash = color.slice(1)
+    // console.log(colorLessHash)
 
-        fetch(`http://www.thecolorapi.com/id?hex=${colorLessHash}`)
-        .then(resp => resp.json())
-        .then(obj => {
-    
-        const hex = document.querySelector("#hex")
-        const rgb = document.querySelector("#rgb")
-        const cmyk = document.querySelector("#cmyk")
-            // console.log(obj)
+    fetch(`http://www.thecolorapi.com/id?hex=${colorLessHash}`)
+      .then(resp => resp.json())
+      .then(obj => {
+        const hex = document.querySelector('#hex')
+
+        const rgb = document.querySelector('#rgb')
+        const cmyk = document.querySelector('#cmyk')
+        // console.log(obj)
         hex.textContent = `HEX  ${obj.hex.value}`
-        rgb.textContent = `RGB  ${(obj.rgb.value).slice(3)}`
-        cmyk.textContent = `CMYK  ${(obj.cmyk.value).slice(4)}`
-    })
+        rgb.textContent = `RGB  ${obj.rgb.value.slice(3)}`
 
-    })
+        cmyk.textContent = `CMYK  ${obj.cmyk.value.slice(4)}`
+      })
+    sectColor.style.display = 'contents'
+  })
 
-    const compColors = document.querySelector("#compColors")
-    const compBtn = document.querySelector("#compBtn")
-    compBtn.addEventListener("click", (e) =>{
-        console.log(e)
-        const color = (colorPicker.value).slice(1)
-        console.log(color)
-        const clear = document.querySelector("#compColors")
-        clear.innerHTML = " "
+  const compColors = document.querySelector('#compColors')
+  const quadColors = document.querySelector('#quadColors')
+  const analogicColors = document.querySelector('#analogicColors')
+  const monochromeColors = document.querySelector('#monochromeColors')
+  //   const compBtn = document.querySelector('#compBtn')
+  //   compBtn.addEventListener('click', e => {
+  //     buildCards('quad', compColors)
+  //   })
 
-        fetch(`https://www.thecolorapi.com/scheme?hex=${color}`)
-        .then(resp => resp.json())
-        .then(obj => {
-            console.log(obj)
+  const quadBtn = document.querySelector('#quadBtn')
+  quadBtn.addEventListener('click', e => {
+    buildCards('quad', quadColors)
+    console.log(e)
+  })
 
+  const analogicBtn = document.querySelector('#analogicBtn')
+  analogicBtn.addEventListener('click', () => {
+    buildCards('analogic-complement', analogicColors)
+  })
 
-            for (element of obj.colors){
-                console.log(element.hex.value)
+  const monochromeBtn = document.querySelector('#monochromeBtn')
+  monochromeBtn.addEventListener('click', () => {
+    buildCards('monochrome', monochromeColors)
+  })
 
-                const newDiv = document.createElement("div")
-                newDiv.setAttribute("id", "temp")
+  //   Build Card
+  const buildCards = (mode, div) => {
+    const color = colorPicker.value.slice(1)
+    console.log(color)
+    // const clear = document.querySelector('#compColors')
+    div.innerHTML = ' '
 
-               // newDiv.innerHTML = "<h1>test</h1>"
-                newDiv.innerHTML = `
-                <div class="card mt-5 text-center" style="width: 18rem; height: 18rem;">
-                <div id="comp1" class="card-body" style="background-color: ${element.hex.value}" >
-                  </div>
-                <ul class="list-group list-group-flush">
-                <li class="list-group-item" id="compcmyk">CMYK ${(element.cmyk.value).slice(4)} </li>
-                  <li class="list-group-item" id="comprgb">RGB ${(element.rgb.value).slice(3)}</li>
-                    <li class="list-group-item" id="comphex">HEX ${(element.hex.value)}</li>
-                </ul>
-              </div>
-                
-                
-                `
+    fetch(`https://www.thecolorapi.com/scheme?hex=${color}&mode=${mode}`)
+      .then(resp => resp.json())
+      .then(obj => {
+        console.log(obj)
 
+        for (element of obj.colors) {
+          console.log(element.hex.value)
 
-            
-    
-                compColors.appendChild(newDiv)
-                newDiv.replaceWith(...newDiv.childNodes)
-                
-                
-               
+          const newDiv = document.createElement('div')
+          newDiv.setAttribute('id', 'temp')
 
+          // newDiv.innerHTML = "<h1>test</h1>"
+          newDiv.innerHTML = `
+          <div class="card mt-5 text-center" style="width: 18rem; height: 18rem;">
+          <div id="comp1" class="card-body" style="background-color: ${
+            element.hex.value
+          }" >
+            </div>
+          <ul class="list-group list-group-flush">
+          <li class="list-group-item" id="compcmyk">CMYK ${element.cmyk.value.slice(
+            4
+          )} </li>
+            <li class="list-group-item" id="comprgb">RGB ${element.rgb.value.slice(
+              3
+            )}</li>
+              <li class="list-group-item" id="comphex">HEX ${
+                element.hex.value
+              }</li>
+          </ul>
+        </div>
+          `
+          div.appendChild(newDiv)
+          newDiv.replaceWith(...newDiv.childNodes)
+        }
+      })
+  }
 
-           
-            
-                
-
-
-            }
-        })
-
-    })
-
-
-
-
-
-
-
-
-
-
-
+  //   closing brackets
 })
