f73c9df51491fcad0bd503f889cc9ab96d1c6a3c
Jimmy Andreas <jimmysignature@gmail.com>
2022-05-15 22:03:09 -0400

    Added stlying and cards refresh

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 window.addEventListener('DOMContentLoaded', e => {
-  console.log('connected')
-
   const selectedColor = document.querySelector('#selectedColor')
   const colorPicker = document.querySelector('#colorPicker')
-
   const sectColor = document.querySelector('#sectColor')
   sectColor.style.display = 'none'
 
   colorPicker.addEventListener('change', e => {
     const color = e.target.value
-    // console.log(e.target.value)
     selectedColor.style.backgroundColor = color
     const colorLessHash = color.slice(1)
-    // console.log(colorLessHash)
 
     fetch(`http://www.thecolorapi.com/id?hex=${colorLessHash}`)
       .then(resp => resp.json())
@@ -21,45 +16,25 @@ window.addEventListener('DOMContentLoaded', e => {
 
         const rgb = document.querySelector('#rgb')
         const cmyk = document.querySelector('#cmyk')
-        // console.log(obj)
+
         hex.textContent = `HEX  ${obj.hex.value}`
         rgb.textContent = `RGB  ${obj.rgb.value.slice(3)}`
 
         cmyk.textContent = `CMYK  ${obj.cmyk.value.slice(4)}`
       })
     sectColor.style.display = 'contents'
-  })
 
-  const compColors = document.querySelector('#compColors')
-  const quadColors = document.querySelector('#quadColors')
-  const analogicColors = document.querySelector('#analogicColors')
-  const monochromeColors = document.querySelector('#monochromeColors')
-  //   const compBtn = document.querySelector('#compBtn')
-  //   compBtn.addEventListener('click', e => {
-  //     buildCards('quad', compColors)
-  //   })
-
-  const quadBtn = document.querySelector('#quadBtn')
-  quadBtn.addEventListener('click', e => {
+    const quadColors = document.querySelector('#quadColors')
     buildCards('quad', quadColors)
-    console.log(e)
-  })
-
-  const analogicBtn = document.querySelector('#analogicBtn')
-  analogicBtn.addEventListener('click', () => {
+    const analogicColors = document.querySelector('#analogicColors')
     buildCards('analogic-complement', analogicColors)
-  })
-
-  const monochromeBtn = document.querySelector('#monochromeBtn')
-  monochromeBtn.addEventListener('click', () => {
+    const monochromeColors = document.querySelector('#monochromeColors')
     buildCards('monochrome', monochromeColors)
   })
 
   //   Build Card
   const buildCards = (mode, div) => {
     const color = colorPicker.value.slice(1)
-    console.log(color)
-    // const clear = document.querySelector('#compColors')
     div.innerHTML = ' '
 
     fetch(`https://www.thecolorapi.com/scheme?hex=${color}&mode=${mode}`)
@@ -72,27 +47,55 @@ window.addEventListener('DOMContentLoaded', e => {
 
           const newDiv = document.createElement('div')
           newDiv.setAttribute('id', 'temp')
-
-          // newDiv.innerHTML = "<h1>test</h1>"
           newDiv.innerHTML = `
-          <div class="card mt-5 text-center" style="width: 18rem; height: 18rem;">
-          <div id="comp1" class="card-body" style="background-color: ${
+          <div class="card mt-5 text-center" style="width: 15rem; height: auto;">
+          <div id="comp1" class="card-body mt-2 p-5 ;" style="background-color: ${
             element.hex.value
           }" >
             </div>
           <ul class="list-group list-group-flush">
-          <li class="list-group-item" id="compcmyk">CMYK ${element.cmyk.value.slice(
+          
+          <li class="list-group-item" id="compcmyk"><span>CMYK ${element.cmyk.value.slice(
             4
-          )} </li>
-            <li class="list-group-item" id="comprgb">RGB ${element.rgb.value.slice(
+          )} <i class="bi bi-clipboard"></i></span></li>
+          
+          
+            <li class="list-group-item" id="comprgb"><span>RGB ${element.rgb.value.slice(
               3
-            )}</li>
-              <li class="list-group-item" id="comphex">HEX ${
+            )} <i class="bi bi-clipboard"></i></span></li>
+              <li class="list-group-item" id="comphex"><span>HEX ${
                 element.hex.value
-              }</li>
+              } <i class="bi bi-clipboard"></i></span></li>
+
+
+
+              
+              <div class="input-group mb-3">
+                 <span class="input-group-text">CMYK</span>
+                    <input type="text" class="form-control">
+                <span class="input-group-text"><i class="bi bi-clipboard"></i></span>
+              </div>
+              <div class="input-group mb-3">
+                 <span class="input-group-text">RGB</span>
+                    <input id="cardBuildText" type="text" value=${element.rgb.value.slice(
+                      3
+                    )} 
+                class="form-control ">
+                <span class="input-group-text"><i class="bi bi-clipboard"></i></span>
+              </div>
+              <div class="input-group mb-3">
+                 <span class="input-group-text">HEX</span>
+                    <input type="text" value=${
+                      element.hex.value
+                    } class="form-control">
+                <span class="input-group-text"><i class="bi bi-clipboard"></i></span>
+              </div>
+              
           </ul>
         </div>
           `
+
+          //   this removes div to make bootstrap work
           div.appendChild(newDiv)
           newDiv.replaceWith(...newDiv.childNodes)
         }
